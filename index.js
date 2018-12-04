b01df99b4973e3e5080cce71e27324c5eeecf66e
Matthieu Brouillard <mbrouilla@gmail.com>
2018-12-04 09:19:56 +0100

    Fix module.exports

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express"); // import the NPM dependancy package
+
+const app = express(); // initialise express() inside and write to the app variable
+
+require("./routes/userRoutes")(app); // import route module and pass your app
+
+const PORT = 5000; // choose what port on which to run the server
+
+app.listen(PORT, () => { // use the app variable and listen on the port
+    console.log(`Server running`);
+});
---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+const jsonfile = require("jsonfile");
+
+module.exports = app => {
+    app.get("/users", (req, res) => {
+        console.log("fetching all users");
+
+        // jsonfile reading
+        jsonfile.readFile("./DB/users.json", function (err, content) {
+            // send file contents back to sender
+            res.send(content);
+        });
+    });
+}
